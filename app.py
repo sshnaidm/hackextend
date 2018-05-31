b90b04b3513ee788dedbf0f76b87ae17da2163df
Sagi Shnaidman <sshnaidm@redhat.com>
2018-05-31 22:04:02 +0300

    Ready

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, send_from_directory
 import os
 from skills import get_info
 from flask_bootstrap import Bootstrap
+from draw import run
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -13,13 +14,17 @@ def index():
     if request.method == 'POST':
         link = request.form.get('linkedin')
         skills_years = get_info(link)
-
+        result = run(skills_years)
+        return str(result)
         #return render_template('months.html.j2', skills=skills_years)
     elif request.method == 'GET':
         return render_template('form.html.j2', message=message)
     else:
         abort(404)
 
+@app.route('/pics/<path:path>')
+def send_js(path):
+    return send_from_directory('static/pics', path)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),
--- draw.py
+++ draw.py
@@ -6,6 +6,10 @@ import requests
 from time import sleep
 from PIL import Image, ImageDraw
 
+
+PICS = 'static/pics/'
+DMN = 'https://techshirt-sergeysh.c9users.io/pics'
+
 #logos = ["red", "green", "blue", "orange", "pink", "purple", "yellow", "cyan"]
 #logos = ["android", "angular", "ansible", "docker", "kubernetes", "python", "react", "vue", "puppet", "grafana", "redis", "linux", "prometheus", "apache", "django"]
 techs = os.listdir("logo")
@@ -34,13 +38,18 @@ left_places = [((50, 50), 700)]
 right_places = [((50, 50), 700)]
 
 
-generated_urls = {"default" : "https://image.ibb.co/d01nFy/front.png",
-        "back" : "https://image.ibb.co/eTxr8J/back.png",
-        "sleeve_left" : "https://image.ibb.co/mMM18J/left.png",
-        "sleeve_right" : "https://image.ibb.co/hYeooJ/right.png"}
+# generated_urls = {"default" : "https://image.ibb.co/d01nFy/front.png",
+#         "back" : "https://image.ibb.co/eTxr8J/back.png",
+#         "sleeve_left" : "https://image.ibb.co/mMM18J/left.png",
+#         "sleeve_right" : "https://image.ibb.co/hYeooJ/right.png"}
+
+generated_urls = {"default" : DMN + "front.png",
+        "back" : DMN + "back.png",
+        "sleeve_left" : DMN + "left.png",
+        "sleeve_right" : DMN + "right.png"}
 
 def send_shirt(urls):
-    with open('tshirt.json', encoding='utf-8') as data_file:
+    with open('tshirt.json') as data_file:
         data = json.loads(data_file.read())
     for fileim in data["files"]:
         fileim["image_url"] = urls[fileim["placement"]]
@@ -68,7 +77,7 @@ def wait_shirt(task_id):
     shirt_mockups.append(result["mockup_url"])
     for extra in result["extra"]:
         shirt_mockups.append(extra["url"])
-    print(shirt_mockups)
+    return shirt_mockups
 
 
 def draw_logo(input_path, logo_files, places, output_path):
@@ -76,19 +85,23 @@ def draw_logo(input_path, logo_files, places, output_path):
     for place, size in places:
         if (len(logo_files) < 1):
             break
-        index = random.randint(0, len(logo_files) - 1)
-        logo_file = logo_files.pop(index).resize((size, size), Image.BICUBIC)
+        #index = random.randint(0, len(logo_files) - 1)
+        logo_file = logo_files.pop().resize((size, size), Image.BICUBIC)
         image.paste(logo_file, place, logo_file)
     image.save(output_path, "PNG")
 
 
-def shirt():
-    logo_files = list(map(lambda logo: Image.open("logo/"+logo).convert('RGBA'), techs))
-    draw_logo("empty_frontback.png", logo_files, front_places, "front.png")
-    draw_logo("empty_frontback.png", logo_files, back_places, "back.png")
-    draw_logo("empty_side.png", logo_files, left_places, "left.png")
-    draw_logo("empty_side.png", logo_files, right_places, "right.png")
+def shirt(techs):
+    logo_files = list(map(lambda logo: Image.open("logo/"+logo + ".png").convert('RGBA'), techs))
+    draw_logo("empty_frontback.png", logo_files, front_places, PICS + "front.png")
+    draw_logo("empty_frontback.png", logo_files, back_places, PICS + "back.png")
+    draw_logo("empty_side.png", logo_files, left_places, PICS + "left.png")
+    draw_logo("empty_side.png", logo_files, right_places, PICS + "right.png")
+
+def run(t):
+    shirt(t)
+    wait_shirt(send_shirt(generated_urls))
 
 if __name__ == "__main__":
-    shirt()
+    shirt(techs)
     wait_shirt(send_shirt(generated_urls))
