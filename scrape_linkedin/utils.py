1bae39bfe9056fc517f04b1fd0c0e426451a13fd
Sagi Shnaidman <sshnaidm@redhat.com>
2018-05-31 20:10:30 +0300

    Additional settings for chrome

--- scrape_linkedin/utils.py
+++ scrape_linkedin/utils.py
@@ -3,6 +3,10 @@ import math
 
 options = Options()
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--no-first-run')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-extensions')
 HEADLESS_OPTIONS = {'chrome_options': options}
 
 
